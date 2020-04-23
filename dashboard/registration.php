98cec467ae20edefb8df7442a7cf129032265fbd
Lukas <lukasrichters14@gmail.com>
2020-04-22 23:43:57 -0400

    Created basic dashboard to test API.

---
+++ dashboard/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+// Checks if the form is submitted
+if ( isset( $_POST['submit'] ) ) {
+    // Retrieves the form data.
+    $rcode = $_POST['rcode'];
+    $mac_addr = $_POST['mac_addr'];
+
+?>
