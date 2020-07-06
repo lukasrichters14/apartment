b43db2c9ffc438e2dd50ee34df2c17000af40d83
Lukas <lukasrichters14@gmail.com>
2020-07-06 14:24:55 -0400

    Created new angular project for the dashboard.

--- api.py
+++ api.py
@@ -11,13 +11,19 @@ def home():
     pass
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
     """
     Registers a user with the network.
     :return:
     """
-    pass
+    # Ensure registration code and mac address are in the request
+    if "code" in request.args and "mac" in request.args:
+        code = str(request.args["code"])
+        mac = str(request.args["mac"])
+    # Error if the API call is incorrect.
+    else:
+        return "Error: No id field provided. Please specify an id."
 
 
 if __name__ == '__main__':
