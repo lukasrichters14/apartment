294a9e237de65a096b922f6e3e84e41e7df6b9df
Lukas <lukasrichters14@gmail.com>
2020-07-06 16:29:06 -0400

    Login component added.

--- apartment-dashboard/src/app/app-routing.module.ts
+++ apartment-dashboard/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MenuComponent} from "./menu/menu.component";
+import {LoginComponent} from "./login/login.component";
 
 
 const routes: Routes = [
-    {path: '', component: MenuComponent}
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
