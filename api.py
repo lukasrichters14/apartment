0a41b54ce7814eb2f572c6cb2ebcabcf37482964
Lukas <lukasrichters14@gmail.com>
2020-07-14 15:58:37 -0400

    API updated to use the users database for registration.

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import sqlite3
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
-USERS_DB = 'users.db'
+USERS_DB = './data/users.db'
 
 
 def create_dict(cursor, row):
@@ -22,6 +22,40 @@ def create_dict(cursor, row):
     return d
 
 
+def query_db(db, query):
+    """
+    Performs an SQL query on a database.
+    :param db: [string] the file path to the database.
+    :param query: [string] an SQL-formatted query to the database.
+    :return: [dict] the SQL response from the query.
+    """
+    # Connect to the database.
+    database = sqlite3.connect(db)
+    # Set the format of the return of the SQL query.
+    database.row_factory = create_dict
+    cursor = database.cursor()
+    result = cursor.execute(query).fetchall()
+    cursor.close()
+    return result
+
+
+def modify_db(db, query):
+    """
+    Modifies the given database according to the given command.
+    :param db: [string] the database to modify.
+    :param query: [string] the SQL command to execute.
+    :return: None.
+    """
+    # Connect to the database.
+    database = sqlite3.connect(db)
+    cursor = database.cursor()
+    # Execute the command.
+    cursor.execute(query)
+    # Commit the changes.
+    database.commit()
+    cursor.close()
+
+
 def authenticated(username, token):
     """
     Checks user database for the provided token.
@@ -29,14 +63,8 @@ def authenticated(username, token):
     :param token: [string] the token granted to current users.
     :return: [bool] True if the username/token combination is valid, False otherwise.
     """
-    # Connect to the users database.
-    database = sqlite3.connect(USERS_DB)
-    # Set the format of the return of the SQL query.
-    database.row_factory = create_dict
-    cursor = database.cursor()
-    # Form the SQL query.
     query = ''
-    result = cursor.execute(query).fetchall()
+    result = query_db(USERS_DB, query)
     # There is a user with the given user and token combination, they are authenticated.
     if result:
         return True
@@ -60,7 +88,41 @@ def response(error=False, msg="1"):
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Apartment API"
+    return "Apartment API. Created by Lukas Richters, 2020."
+
+
+def check_registration_code(code):
+    """
+    Ensures the code is formatted correctly.
+    :param code: [string] the code to check.
+    :return: [bool] True if the code is formatted correctly, False otherwise.
+    """
+    # All codes are 5 5-letter words separated by hyphens. So, a valid code has 29 characters and
+    # the 6th, 12th, 18th, and 24th characters are hyphens.
+    if len(code) != 29:
+        return False
+
+    elif code[5] != '-' or code[11] != '-' or code[17] != '-' or code[23] != '-':
+        return False
+
+    return True
+
+
+def valid_registration_code(code):
+    """
+    Checks the given registration code for existence in the database.
+    :param code: [string] the code to check for.
+    :return: [bool] True if the code is in the database, False otherwise.
+    """
+
+    # Ensure code doesn't have malicious SQL in it.
+    if check_registration_code(code):
+        query = 'SELECT code FROM registration_codes WHERE code=' + code + ';'
+        result = query_db(USERS_DB, query)
+        if len(result) == 1:
+            return True
+
+    return False
 
 
 @app.route('/register', methods=['POST'])
@@ -69,14 +131,27 @@ def register():
     Registers a user with the network.
     :return: [string] a JSON object with an error or success message.
     """
-    # Ensure registration code is in the request.
-    if "code" in request.args:
+    # Ensure registration code, name, and email are provided in the request.
+    if "code" in request.args and "name" in request.args and "email" in request.args:
         code = str(request.args["code"])
-    # Error if no code.
+        name = str(request.args["name"])
+        email = str(request.args["email"])
+    # Error if any field is unfilled.
     else:
-        return response(error=True, msg="No code provided.")
+        return response(error=True, msg="Insufficient information.")
 
-    # TODO: check that the given code is in the database.
+    # Check that the code is valid.
+    if valid_registration_code(code):
+        # Remove the registration code from the database so it cannot be used again.
+        query = 'DELETE FROM registration_codes WHERE code=' + code + ';'
+        modify_db(USERS_DB, query)
+
+        # Add user to the database.
+        query = 'INSERT INTO users VALUES (' + name + ', ' + email + ')'
+        modify_db(USERS_DB, query)
+
+    else:
+        return response(error=True, msg="The provided code is invalid.")
 
 
 if __name__ == '__main__':
